c9464db75628bd435f276addc8b83f8c5faafd71
SiLViU <silviuptc@gmail.com>
2025-11-18 17:28:34 +0200

    Created Cars servlet

--- src/main/java/org/example/parkinglot/servlets/Cars.java
+++ src/main/java/org/example/parkinglot/servlets/Cars.java
@@ -1,10 +1,8 @@
 package org.example.parkinglot.servlets;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
 
 import java.io.IOException;
 
